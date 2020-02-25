09a4c0dbb8128744f3a5bd7d9ef8f2086eb36767
mattrhansen <mattrhansen@gmail.com>
2020-02-25 05:57:34 +0000

    Initial Commit

--- app.rb
+++ app.rb
@@ -6,13 +6,25 @@ require "httparty"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-# enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "3346c332175cc193b8f51b05c6fc0e39"
 
 get "/" do
-  # show a view that asks for the location
+    @entry = false
+    view "ask"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+
+    @lat_long = results.first.coordinates # => [lat, long]
+
+    @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
+
+    url = "https://newsapi.org/v2/top-headlines?country=#{results.first.country_code}&apiKey=7f70e635bcf841fbac13e7e5d4f6f196"
+
+    @news = HTTParty.get(url).parsed_response.to_hash
+    
+    @entry = true
+
+    view "ask"
 end
